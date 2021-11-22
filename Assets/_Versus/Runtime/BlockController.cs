382a9a015aab5c5935cb8cfed9f3db2b318f723c
Ross Gardler <ross@gardler.org>
2021-11-21 17:43:53 -0800

    more efficient and more varied calculation of the influence colour

--- Assets/_Versus/Runtime/BlockController.cs
+++ Assets/_Versus/Runtime/BlockController.cs
@@ -14,6 +14,10 @@ namespace WizardsCode.Versus.Controller
         public BlockType BlockType;
         [SerializeField, Tooltip("The mesh that will show the faction control.")]
         Transform m_FactionMap;
+        [SerializeField, Tooltip("The frequency, in seconds, the faction map should be updated. This is a costly operation on large maps so don't make it too frequent.")]
+        float m_FactionMapUpdateFrequency = 1f;
+        [SerializeField, Tooltip("The number of excess faction members that need to be present for dominance. This is used to calculate a factions influence on the block. If a faction has a majority of this many on a block then it is considered to have dominance.")]
+        int m_FactionMembersNeededForControl = 5;
 
         private Mesh m_FactionMesh;
         private CityController cityController;
@@ -21,33 +25,7 @@ namespace WizardsCode.Versus.Controller
         private List<AnimalController> m_DogsPresent = new List<AnimalController>();
         private List<AnimalController> m_CatsPresent = new List<AnimalController>();
 
-        public float CatInfluence
-        {
-            get
-            {
-                if (m_CatsPresent.Count + m_DogsPresent.Count > 0)
-                {
-                    return m_CatsPresent.Count / (m_CatsPresent.Count + m_DogsPresent.Count);
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-        public float DogInfluence
-        {
-            get
-            {
-                if (m_CatsPresent.Count + m_DogsPresent.Count > 0)
-                {
-                    return m_DogsPresent.Count / (m_CatsPresent.Count + m_DogsPresent.Count);
-                } else
-                {
-                    return 0;
-                }
-            }
-        }
+        private float timeOfNextFactionMapUpdate = 0;
 
         private void Start()
         {
@@ -69,14 +47,18 @@ namespace WizardsCode.Versus.Controller
 
         private void Update()
         {
-            //OPTIMIZATION: probably update the faction mesh on a longer cycle than every frame
+            if (Time.timeSinceLevelLoad < timeOfNextFactionMapUpdate) return;
+
+            timeOfNextFactionMapUpdate = m_FactionMapUpdateFrequency + Time.timeSinceLevelLoad;
+
             //OPTIMIZATION: if this is not in view of the camera there is no need to update the faction mesh
             Vector3[] vertices = m_FactionMesh.vertices;
             Color32[] colors = new Color32[vertices.Length];
 
+            Color32 blockColor = cityController.m_FactionGradient.Evaluate(NormalizedFactionInfluence);
             for (int i = 0; i < vertices.Length; i++)
             {
-                colors[i] = cityController.m_FactionGradient.Evaluate(NormalizedFactioninfluence);
+                colors[i] = blockColor;
             }
 
             m_FactionMesh.colors32 = colors;
@@ -87,21 +69,23 @@ namespace WizardsCode.Versus.Controller
         /// 0.5 is neutral, 0 is cat controlled, 1 is dog controlled
         /// </summary>
         /// <returns>0.5 is neutral, 0 is cat controlled, 1 is dog controlled</returns>
-        internal float NormalizedFactioninfluence
+        internal float NormalizedFactionInfluence
         {
             get
             {
-                float influence = 0.5f;
-                if (DogInfluence > CatInfluence)
+                if (m_DogsPresent.Count == m_CatsPresent.Count)
                 {
-                    influence = (DogInfluence - CatInfluence) / 2 + 0.5f;
-                }
-                else if (CatInfluence > DogInfluence)
+                    return 0.5f;
+                } else if (m_DogsPresent.Count > m_CatsPresent.Count)
                 {
-                    influence = 0.5f - (CatInfluence - DogInfluence) / 2;
+                    float influence = (float)(m_DogsPresent.Count - m_CatsPresent.Count) / m_FactionMembersNeededForControl;
+                    return 0.5f + influence;
                 }
-
-                return influence;
+                else
+                {
+                    float influence = (float)(m_CatsPresent.Count - m_DogsPresent.Count) / m_FactionMembersNeededForControl;
+                    return 0.5f - influence;
+                }
             }
         }
     }
