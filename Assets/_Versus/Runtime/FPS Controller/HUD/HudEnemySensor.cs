cc049ae70d1bf08bf4d2836c531e1c41311caf9f
Ross Gardler <ross@gardler.org>
2021-12-07 23:43:11 -0800

    Add some rudimentary markers to the hud to indicate enemy position. Closes #59

---
+++ Assets/_Versus/Runtime/FPS Controller/HUD/HudEnemySensor.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using WizardsCode.Versus.Controller;
+using NeoFPS;
+using WizardsCode.Versus.FPS;
+using UnityEngine.UI;
+
+namespace WizardsCode.Versus
+{
+    public class HudEnemySensor : PlayerCharacterHudBase
+    {
+        [SerializeField, Tooltip("The enemy marker prefab to use on the HUD to indicate an enemy.")]
+        Image m_EnemyMarkerPrefab;
+        [SerializeField, Tooltip("Should markers be clamped to the screen?")]
+        bool clampToScreen = true;
+        [SerializeField, Tooltip("If clamping to the the screen what border size should we use.")] 
+        Vector2 clampBorderSize = new Vector2(10, 10);
+        [SerializeField, Tooltip("The offset should be used to ensure the enemy marker is position correctly.")]
+        Vector3 offset = Vector3.zero;
+
+        List<Image> m_Markers = new List<Image>();
+        BlockController m_Block;
+        PlayerCharacter player;
+        private Camera mainCamera;
+
+        protected override void Start()
+        {
+            base.Start();
+            mainCamera = Camera.main;
+        }
+
+        public override void OnPlayerCharacterChanged(ICharacter character)
+        {
+            if (character == null) return;
+
+            player = ((PlayerCharacter)character);
+            player.OnCurrentBlockChanged += OnCurrentBlockChanged;
+            OnCurrentBlockChanged(player.CurrentBlock);
+        }
+
+        private void OnCurrentBlockChanged(BlockController newBlock)
+        {
+            if (newBlock)
+            {
+                m_Block = newBlock;
+            }
+            else
+            {
+                m_Block = null;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (!m_Block) return;
+
+            for (int i = 0; i < m_Block.GetEnemiesOf(AnimalController.Faction.Cat).Count || i < m_Markers.Count; i++)
+            {
+                if (i >= m_Block.GetEnemiesOf(AnimalController.Faction.Cat).Count)
+                {
+                    m_Markers[i].gameObject.SetActive(false);
+                }
+                else
+                {
+                    if (i >= m_Markers.Count)
+                    {
+                        m_Markers.Add(Instantiate(m_EnemyMarkerPrefab, gameObject.transform));
+                    }
+                    m_Markers[i].gameObject.SetActive(true);
+                    Vector3 noClampPosition = mainCamera.WorldToScreenPoint(m_Block.GetEnemiesOf(AnimalController.Faction.Cat)[i].transform.position + offset);
+
+                    m_Markers[i].rectTransform.position = new Vector3(Mathf.Clamp(noClampPosition.x, 0 + clampBorderSize.x, Screen.width - clampBorderSize.x),
+                                                                    Mathf.Clamp(noClampPosition.y, 0 + clampBorderSize.y, Screen.height - clampBorderSize.y),
+                                                                      noClampPosition.z);
+                }
+            }
+        }
+    }
+}
